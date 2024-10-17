a1992586c03f54170deb144830560e59860946bc
Aya Mohamed <152428721+fou1100ouacus@users.noreply.github.com>
2024-10-17 09:39:38 +0300

    Add files via upload

---
+++ SortingList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // using built in fun in list
+            List<int> list = new List<int>() {43,54,6,2,6,234,64,32 };
+         //   list.Sort();
+            Console.WriteLine(string.Join(",",list));
+        //    list.Reverse();
+            Console.WriteLine(string.Join(",", list));
+
+            //   using linq
+          //  list.OrderBy(x => x);
+            Console.WriteLine(string.Join(",", list));
+            list.OrderByDescending(x => x);
+            Console.WriteLine(string.Join(",", list));
+
+            // Using custom sorting logic
+            list.Sort((a,b)=>Math.Abs(a).CompareTo(b));
+
+
+        }
+    }
+}
