d14357f21b535d472ced48c649a48a39ad098423
Aya Mohamed <152428721+fou1100ouacus@users.noreply.github.com>
2024-10-17 09:40:56 +0300

    Add files via upload

---
+++ HashTable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable table=new Hashtable();
+            table.Add("key1", "value1");
+            table.Add("key2", "vafdfslue1");
+            table.Add("key3", "value1");
+            table.Add("key4", "value1");
+            table.Add("key5", "value1");
+
+            Console.WriteLine(table["key2"]);
+            table.Remove("key1");
+            foreach (DictionaryEntry item in table)
+            {
+                Console.WriteLine(item.Key +" - "+item.Value);
+            }
+        }
+    }
+}
