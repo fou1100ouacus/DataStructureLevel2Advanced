5c052313b871fac9ebca78ea2fe8543ff061d061
Aya Mohamed <152428721+fou1100ouacus@users.noreply.github.com>
2024-10-17 09:38:41 +0300

    Add files via upload

---
+++ Boxing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Boxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;// stored in stack
+            object intobj = x; // stored in heap
+            Console.WriteLine(x);
+            Console.WriteLine(intobj);
+        }
+    }
+}
