d38880e168abf269710a6b6504d3823bda919feb
Aya Mohamed <152428721+fou1100ouacus@users.noreply.github.com>
2024-10-17 09:39:11 +0300

    Add files via upload

---
+++ UnBoxing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnBoxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;// stored in stack
+            object intobj = x; // stored in heap
+            Console.WriteLine(x);// boxing
+            int unboxing=(int)intobj;
+            Console.WriteLine(unboxing);
+
+            Console.WriteLine(intobj);
+        }
+    }
+}
