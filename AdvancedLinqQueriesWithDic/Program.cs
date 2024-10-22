0c3766e0d2ff8672552721d107e51e6208144ed7
Aya Mohamed <152428721+fou1100ouacus@users.noreply.github.com>
2024-10-22 11:28:35 +0300

    Add files via upload

---
+++ AdvancedLinqQueriesWithDic/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedLinqQueriesWithDic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Dictionary<string,string> dic =
+                new  Dictionary<string, string>
+                {
+
+                    {"apple","tree" },
+                    {"banana","herb" },
+                    {"cherry","tree" },
+                    {"strww","bush" },
+                    {"rasbry","bush" },
+
+                };
+
+
+
+            var groupedFruit = dic.GroupBy(x => x.Value);
+
+
+            foreach (var group in groupedFruit)
+            {
+                Console.WriteLine("-----------");
+
+                foreach (var item in group)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+            Console.WriteLine("--------------------------------");
+
+
+
+        }
+    }
+}
