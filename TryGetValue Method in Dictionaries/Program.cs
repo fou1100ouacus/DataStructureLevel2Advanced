4edce11157f421bb56ea6877b3dcc4160aca3b29
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2024-12-29 08:32:56 +0200

    Add files via upload

---
+++ TryGetValue Method in Dictionaries/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryGetValue_Method_in_Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary <string,int> dic=new Dictionary<string,int>();
+            dic.Add("1", 4);
+            dic.Add("2", 5);
+            dic.Add("3", 6);
+            dic.Add("4", 7);
+            dic.Add("5", 8);
+            dic.Add("6", 9);
+            if(dic.TryGetValue("1", out int val) )
+            {
+                Console.WriteLine($"The value is {val} ");
+            }
+            else { Console.WriteLine("Not found ");
+            }
+        }
+
+    }
+}
