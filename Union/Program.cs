f9e197a143847dafb1ab80618b4a6a3585453ef8
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2024-12-29 08:33:53 +0200

    Add files via upload

---
+++ Union/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Union
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set1 = new HashSet<int> { 1, 2, 3 };
+            HashSet<int> set2 = new HashSet<int> { 4, 5, 6 };
+            set1.UnionWith(set2);
+            foreach (int i in set1) { Console.WriteLine(i); }
+            Console.ReadKey();
+            //HashSet<int> set1 = new HashSet<int> { 1, 4, 3 };
+            //HashSet<int> set2 = new HashSet<int> { 4, 5, 6 };
+            //set1.IntersectWith(set2);
+            //foreach (int i in set1) { Console.WriteLine(i); }
+            //Console.ReadKey();
+
+            HashSet<int> set11 = new HashSet<int> { 1, 4, 3 };
+            HashSet<int> set12 = new HashSet<int> { 4, 5, 6 };
+            set11.ExceptWith(set12);
+            foreach (int i in set11) { Console.WriteLine(i); }//
+            Console.ReadKey();
+        
+            
+            HashSet<int> set1q = new HashSet<int> { 1, 4, 3 };
+            HashSet<int> set2q = new HashSet<int> { 4, 5, 6 };
+            set1q.SymmetricExceptWith(set2q);
+            foreach (int i in set1q)
+            { Console.WriteLine(i); }
+            Console.ReadKey();
+
+
+        }
+    }
+}
